b359c20c2dbc198488e5f51db15f9061ed06b053
Chaitanya <chaitanyapandit@me.com>
2014-12-02 20:42:59 +0530

    Added validation for properties that can be archived

--- PFFile+NSCoding.m
+++ PFFile+NSCoding.m
@@ -10,31 +10,62 @@
 #import <objc/runtime.h>
 
 #define kPFFileName @"_name"
-#define kPFFileURL @"_url"
+#define kPFFileIvars @"ivars"
 #define kPFFileData @"data"
 
 @implementation PFFile (NSCoding)
 
 - (void)encodeWithCoder:(NSCoder*)encoder
 {
-	[encoder encodeObject:self.name forKey:kPFFileName];
-    [encoder encodeObject:self.url forKey:kPFFileURL];
-	if (self.isDataAvailable) {
-		[encoder encodeObject:[self getData] forKey:kPFFileData];
-	}
+    [encoder encodeObject:self.name forKey:kPFFileName];
+    [encoder encodeObject:[self ivars] forKey:kPFFileIvars];
+    if (self.isDataAvailable) {
+        [encoder encodeObject:[self getData] forKey:kPFFileData];
+    }
 }
 
 - (id)initWithCoder:(NSCoder*)aDecoder
 {
-	NSString* name = [aDecoder decodeObjectForKey:kPFFileName];
-    NSString* url = [aDecoder decodeObjectForKey:kPFFileURL];
-	NSData* data = [aDecoder decodeObjectForKey:kPFFileData];
-	
-	self = [PFFile fileWithName:name data:data];
-	if (self) {
-        [self setValue:url forKey:@"_url"];
-	}
-	return self;
+    NSString* name = [aDecoder decodeObjectForKey:kPFFileName];
+    NSDictionary* ivars = [aDecoder decodeObjectForKey:kPFFileIvars];
+    NSData* data = [aDecoder decodeObjectForKey:kPFFileData];
+    
+    self = [PFFile fileWithName:name data:data];
+    if (self) {
+        for (NSString* key in [ivars allKeys]) {
+            [self setValue:ivars[key] forKey:key];
+        }
+    }
+    return self;
+}
+
++ (BOOL)canEncodeObject:(id)object
+{
+    BOOL retVal = NO;
+    
+    if ([object respondsToSelector:@selector(encodeWithCoder:)])
+        retVal = YES;
+    
+    return retVal;
+}
+
+- (NSDictionary *)ivars
+{
+    NSMutableDictionary* dict = [[NSMutableDictionary alloc] init];
+    unsigned int outCount;
+    
+    Ivar* ivars = class_copyIvarList([self class], &outCount);
+    for (int i = 0; i < outCount; i++){
+        Ivar ivar = ivars[i];
+        NSString* ivarNameString = [NSString stringWithUTF8String:ivar_getName(ivar)];
+        NSValue* value = [self valueForKey:ivarNameString];
+        if ([PFFile canEncodeObject:value]) {
+            [dict setValue:value forKey:ivarNameString];
+        }
+    }
+    
+    free(ivars);
+    return dict;
 }
 
 @end
